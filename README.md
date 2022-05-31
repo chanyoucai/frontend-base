b7152be252bcb8c7d49020bb5e0efa383b671519
chanyoucai <277240773@qq.com>
2022-05-31 23:26:04 +0800

    init frontend-base for h5

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-# frontend-base
-
-> `node.js`版本推荐使用`14.17.0`,用以兼容`scss`。
-
-### rem 布局
-移动端使用 `rem` 布局，以 `iphone6` 为例，在 `public/index.html` 中设置 `<html lang="" style="font-size: 2.666667vw;">`，相当于 `1rem = 10px` 。
+# base
 
 ## Project setup
 ```
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  css: {
-    // 为便于在Vue组件中使用，全局引入variables.scss中定义的变量
-    loaderOptions: {
-      sass: {
-        additionalData: `@import "@/styles/variables.scss";`
-      }
-    }
-  },
-})
+});
